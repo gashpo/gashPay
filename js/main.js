557a55dabb2b135ba866b83216a534994a82cf98
gashpo <135100108+gashpo@users.noreply.github.com>
2025-05-27 18:28:20 +0800

    update web (service term)

--- js/main.js
+++ js/main.js
@@ -14,56 +14,30 @@ if (scrollVal > 1 ) {
 }
 
 $(document).ready(function() {
-    var windowWidth = window.innerWidth;
-
     // scroll header 樣式
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
-
-        if(windowWidth > 1024){
-            if (scroll > 1) {
-                $('header').css({
-                    'padding':'16px 0',
-                    'background':'#fff',
-                    'box-shadow':'0px 4px 10px rgba(0, 0, 0, 0.05',
-                });
-            } else {
-                $('header').css({
-                    'padding':'30px 0',
-                    'background':'none',
-                    'box-shadow':'none',
-                });
-            };
-
-            // if (scroll > 500) {
-            //     $('header .account a.cta-btn').show();
-            // } else {
-            //     $('header .account a.cta-btn').hide();
-            // };
-
-        }else{
-            if (scroll > 1) {
-                $('header').css({
-                    'padding':'16px 0',
-                    'background':'#fff',
-                    'box-shadow':'0px 4px 10px rgba(0, 0, 0, 0.05',
-                });
-            } else {
-                $('header').css({
-                    'padding':'30px 0',
-                    'background':'none',
-                    'box-shadow':'none',
-                });
-            };
+        if (scroll > 1) {
+            $('header').css({
+                'padding':'16px 0',
+                'background':'#fff',
+                'box-shadow':'0px 4px 10px rgba(0, 0, 0, 0.05',
+            });
+        } else {
+            $('header').css({
+                'padding':'30px 0',
+                'background':'none',
+                'box-shadow':'none',
+            });
         }
     });
-
 
     //mobile nav
 	$('.nav-icon').click(function(){
 		$(this).toggleClass('open');
 	});
 
+    var windowWidth = window.innerWidth;
     
     if(windowWidth < 1024){
     }else{
@@ -77,7 +51,7 @@ $(document).ready(function() {
     });
     
 
-    //header nav
+    //header
     $('.about').click(function(){
         $('nav.open').removeClass('open');
         $('button.nav-icon').removeClass('open');
@@ -101,43 +75,6 @@ $(document).ready(function() {
         $('html, body').animate({
             scrollTop: $('#starter').offset().top
         }, 800);
-    });
-
-
-    //聯絡我們 btn
-    $('a.cta-btn').click(function(){
-        $('nav.open').removeClass('open');
-        $('button.nav-icon').removeClass('open');
-        $('body').removeClass('hidden');
-        $('html, body').animate({
-            scrollTop: $('#starter').offset().top
-        }, 800);
-    });
-
-
-    //language
-    $(".language button").click(function(){
-        $(".language ul").delay().slideToggle();
-        $(".ic-arrow").toggleClass("rotate-180");
-    })
-    $(".language a").click(function(){
-        $(".language ul").hide();
-        $(".ic-arrow").removeClass("rotate-180");
-    })
-
-
-    // pop up 彈窗
-    $('button.cta-btn').click(function(){
-        let value = $(this).attr('for')
-        $('#' + value).addClass('active')
-        $('#' + value).show()
-        $('body').css('overflow', 'hidden')
-    });
-
-    $('.popUp button').click(function(){
-        $('.popUp').removeClass('active')
-        $('.popUp').hide()
-        $('body').css('overflow', 'auto')
     });
 });
 
